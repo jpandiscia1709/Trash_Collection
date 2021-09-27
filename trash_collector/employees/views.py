fef3001ccd116e4fdb5924813bacee5fa421421f
jpandiscia1709 <jpangithub1709@gmail.com>
2021-09-27 18:00:00 -0400

    working on Employee model

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -11,3 +11,8 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+# return a lsit of today's customers
+# filter the list for costomers in my zip code
+# filter list for scheduled pickup of today and one time pickup of today
+# exclude suspended accounts
+# exclude where trash has already been pick up  
