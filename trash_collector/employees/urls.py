e5240221110e48165582308f031350fcd89e43a7
ParkerHadley <89861123+ParkerHadley@users.noreply.github.com>
2021-09-29 14:00:58 -0600

    merge conflist resolution

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from .import views
 
 # TODO: Determine what distinct pages are required for the user stories, add a path for each in urlpatterns
 
